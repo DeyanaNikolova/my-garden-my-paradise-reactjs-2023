54ad4f2e94901f285a5fbe32e4d7cc9fdd1a5884
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-03-22 13:25:23 +0000

    Add authentication  service

---
+++ src/services/authService.js
@@ -0,0 +1,19 @@
+import { requestFactory } from "./requester";
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const authService = (token) => {
+    const request = requestFactory(token);
+
+    const login = (data) => request.post(`${baseUrl}/login`, data);
+
+    const register = (data) => request.post(`${baseUrl}/register`, data);
+
+    const logout = () => request.get(`${baseUrl}/logout`);
+
+    return {
+        login,
+        register,
+        logout
+    };
+};
