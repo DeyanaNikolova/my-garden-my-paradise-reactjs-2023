7ef6f22ab18d1d0daf3d0dd819e734ef172d019f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-03-22 21:24:40 +0000

    Add images

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ export const Footer = () => {
     return (
         <div className="footer-box">
             <span>All rights reserved &copy;</span>
-            <a href="/facebook"><img src="/icon/facebook.png" alt="facebook" /></a>
-            <a href="/twitter"><img src="/icon/Twitter.png" alt="twitter" /></a>
-            <a href="linkedin"><img src="/icon/linkedin.png" alt="linkedin" /></a>
-            <a href="/instagram"><img src="/icon/instagram.png" alt="instagram" /></a>
+            <a href="/facebook"><img src="/icon/facebook.48.png" alt="facebook" /></a>
+            <a href="/twitter"><img src="/icon/twitter.48.png" alt="twitter" /></a>
+            <a href="linkedin"><img src="/icon/linkedin.48.png" alt="linkedin" /></a>
+            <a href="/instagram"><img src="/icon/instagram.48.png" alt="instagram" /></a>
         </div>
     );
 };
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -6,15 +6,20 @@ export const Posts = ({
 }) => {
     return (
         <section id="posts-page">
-        <h1>All Posts</h1>
+            <h1>All Posts</h1>
+            {posts.length === 0 && (
+                <div className="loader">
+                    <div><img src="/images/loading.gif" alt="#" /></div>
+                </div>
+            )}
 
-        {posts.map(x =>
-            <Post key={x._id} {...x} />
-        )}
+            {posts.map(x =>
+                <Post key={x._id} {...x} />
+            )}
 
-        {posts.length === 0 && (
-            <h3 className="no-posts">No posts yet</h3>
-        )}
-    </section>
+            {posts.length === 0 && (
+                <h3 className="no-posts">No posts yet</h3>
+            )}
+        </section>
     );
 };
