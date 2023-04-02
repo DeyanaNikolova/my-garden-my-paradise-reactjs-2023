c5cfd51dff69bc037933baf1e8f33b011e5e53ab
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-04-02 12:52:35 +0100

    Separate posts context from app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import { AuthProvider } from './contexts/AuthContext';
-import { postServiceFactory } from './services/postService';
-
 
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
@@ -15,63 +12,42 @@ import { AddPost } from './components/AddPost/AddPost';
 import { UpdatePost } from './components/UpdatePost/UpdatePost';
 import { PostDetails } from './components/Posts/PostDetails';
 import { RouteGuard } from './components/common/RouteGuard';
+import { PostProvider } from './contexts/PostContext';
+import { PostAuthor } from './components/common/PostAuthor';
 
 
 function App() {
-  const navigate = useNavigate();
-  const [posts, setPosts] = useState([]);
- 
-  const postService = postServiceFactory(auth.accessToken);
- 
-
-  useEffect(() => {
-    postService.getAllPost()
-      .then(result => {
-        setPosts(result);
-      })
-  }, []);
-
-  const onAddPostSubmit = async (postData) => {
-    const newPost = postService.addPost(postData);
-    setPosts(state => [...state, newPost]);
-    navigate('/posts');
-  };
-
-  const onPostUpdateSubmit = async (data) => {
-    const result = await postService.updatePost(data._id, data);
-
-    setPosts(state => state.map(p => p._id === data._id ? result : p));
-    navigate(`/posts/${data._id}`);
-  };
-
-
-
-
 
 
   return (
     <AuthProvider>
-      <div id="box">
-        <Header />
+      <PostProvider>
+        <div id="box">
+          <Header />
 
-        <main id="main-container">
-          <Routes>
-            <Route path='/' element={<Home posts={posts} />} />
-            <Route path='/posts' element={<Posts posts={posts} />} />
-            <Route path='/posts/:postId' element={<PostDetails />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/login' element={<Login />} />
+          <main id="main-container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/posts' element={<Posts />} />
+              <Route path='/posts/:postId' element={<PostDetails />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/login' element={<Login />} />
 
-            <Route element={<RouteGuard />} >
-            <Route path='/update-post/:postId' element={<UpdatePost />} />
-            <Route path='/add-post' element={<AddPost />} />
+              <Route element={<RouteGuard />}>
+                <Route path='/update-post/:postId' element={
+                  <PostAuthor>
+                    <UpdatePost />
+                  </PostAuthor>
+                } />
+                <Route path='/add-post' element={<AddPost />} />
 
-            </Route>
-          </Routes>
-        </main>
+              </Route>
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
+          <Footer />
+        </div>
+      </PostProvider>
     </AuthProvider>
   );
 }
