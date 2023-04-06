f4ffce0a8cbbee5dcfd0c8e07ce0f7901e15b3ee
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-04-06 11:58:08 +0100

    Add paragraph to home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ export const Header = () => {
   return (
     <header>
       <h1><Link className="home" to="/">My Garden</Link></h1>
-      
+
       <nav>
         <Link to="/posts" className="nav-links" >All Posts</Link>
         <Link to="/resources" className="nav-links">Resources</Link>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { LatestPosts} from './LatestPosts';
+import { LatestPosts } from './LatestPosts';
 
 
 const url = 'http://localhost:3030/data/posts';
@@ -7,23 +7,25 @@ export const Home = () => {
     const querySort = encodeURIComponent("_createdOn");
     const [latestPosts, setLatestPosts] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         fetch(`${url}?sortBy=${querySort} desc`)
-        .then(res => res.json())
-        .then(result =>{
-            const sliced = result.splice(0, 3)
-        
-            setLatestPosts(sliced);    
-        });
+            .then(res => res.json())
+            .then(result => {
+                const sliced = result.splice(0, 3)
+
+                setLatestPosts(sliced);
+            });
     }, []);
 
- 
+
     return (
         <section id="garden-home-page">
             <div className="welcome-message">
                 <h3>Welcome to MY Garden - My Paradise</h3>
+                <p>Here you will find posts from the most passionate gardeners around the world. You are more than welcome to join us and share your thoughts.</p>
             </div>
-            <img src="/images/gyufyufyu.png" alt="My Garden" />
+
+            <img id="img-home-page" src="/images/gyufyufyu.png" alt="My Garden" />
 
             <div id="home-page">
                 <h1>Latest Posts</h1>
