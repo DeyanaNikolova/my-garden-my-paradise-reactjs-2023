63628c9553755803d2f0c8ec1e6748ef5e1082e4
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-04-06 11:35:50 +0100

    Add title to resource topic

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -2,13 +2,28 @@
 export const Resources = () => {
     return (
         <section id="resources-page">
-            <h1>Here you can find some useful resources related to gardening and tips for looking after your flowers and plants</h1>
+            <h1>Resources related to gardening and tips for looking after your flowers and plants</h1>
 
             <div className="links">
-               
-                <h3>Here you can find guidance on  <a href="https://www.gardenersworld.com/how-to/grow-plants/how-to-grow-roses/" target="_blank">how to grow differnt types of roses</a></h3>
+                <h2 id="resource-type">Resources for Roses</h2>
 
+                <h3>Find your way to statr growing roses with <a href="https://www.davidaustinroses.co.uk/blogs/news/the-basics-of-growing-roses" target="_blank">The Basics of Growing Roses</a> This guide provides information on how to plant and grow roses</h3>
+                <h3>Another useful resource for roses fans is <a href="https://www.rhs.org.uk/plants/roses/growing-guide" target="_blank">How to grow roses</a></h3>
+                <h3>Here you can find guidance on <a href="https://www.gardenersworld.com/how-to/grow-plants/how-to-grow-roses/" target="_blank">how to grow differnt types of roses</a></h3>
+                <h3>Tips for growing healthy roses from <a href="https://gardens.si.edu/learn/blog/tips-for-growing-healthy-roses/" target="_blank">Smithsonian Gardens</a></h3>
 
+                <h2 id="resource-type">Resources for Peones</h2>
+
+                <h3>Ways to look afret your peonies you can find on <a href="https://www.homesandgardens.com/advice/how-to-grow-peonies" target="_blank">How to grow peonies</a></h3>
+                <h3>Simple rules from the expert Alec White <a href="https://www.theenglishgarden.co.uk/plants/3-rules-growing-perfect-peonies/" target="_blank">3 rules for growing perfect peonies</a></h3>
+                <h3>Horticulture magazine provides information on <a href="https://horticulture.co.uk/peonies/containers/" target="_blank">How To Grow Peonies In Pots</a></h3>
+                <h3> <a href="" target="_blank"></a></h3>
+
+                <h2 id="resource-type">Resources for Wild flowers</h2>
+
+                <h3> <a href="" target="_blank"></a></h3>
+                <h3> <a href="" target="_blank"></a></h3>
+                <h3> <a href="" target="_blank"></a></h3>
             </div>
         </section>
     );
