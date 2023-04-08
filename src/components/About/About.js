bb524574472490c64f66853397c048bdb4f79444
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-04-08 19:17:56 +0100

    Add text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,8 @@ export const About = () => {
                         This blog is actually a collective site from more than one writer.
                         All posts are published by unprofessinal gardeners who suggested their ways of
                         gardening. By sharing thier expirience, you can benefit from their posts and apply their advices in your work.
-                        If you are passionate about growing your own garden, or you have some ideas, you are more than welcome to join us and become a part of this community. </p>
+                        If you are passionate about growing your own garden, or you have some ideas, you are more than welcome to join us and become a part of this community.
+                    </p>
                     <p className="paragraph">
                         Here you can find inspirations, ideas, advice or relative resorces of professional gardening websites or simply a place to share your love to gardening.
                     </p>
@@ -20,7 +21,8 @@ export const About = () => {
                 </div>
                 <div className="content">
                     <p className="paragraph">
-                        The purpose of this blog is to gatter experienced and proven information of gardening  </p>
+                        The purpose of this blog is to gatter people with similar interests and share their practical advices.
+                    </p>
                 </div>
             </div>
         </section>
