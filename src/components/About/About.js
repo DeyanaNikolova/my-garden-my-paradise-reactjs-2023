970542ecf9bb4c9cc7febf7e3b20521edf005438
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-04-08 17:32:42 +0100

    Add text to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,26 @@
 export const About = () => {
     return (
         <section>
-            <div>
+            <div className="about-page">
                 <h1>About Us</h1>
-               <p>The purpose of this blog is to gatter experienced and proven information of gardening  </p>
+                <div className="content">
+                    
+                         <p className="paragraph">
+                            This blog is actually a collective site from more than one writer. 
+                         All posts are published by unprofessinal gardeners who suggested their ways of
+                         gardening. By sharing thier expirience, you can benefit from their posts and apply their advices in your work.
+                          If you are passionate about growing your own garden, or you have some ideas, you are more than welcome to join us and become a part of this community. </p>
+                    
+               
+                </div>
+                <div className="image">
+                <img src="/images/apple-blossom.jpg" alt="apple blossom" />
+              
+
+                </div>
+                <div className="content">
+                <p className="paragraph">The purpose of this blog is to gatter experienced and proven information of gardening  </p>
+                </div>
             </div>
         </section>
     );
