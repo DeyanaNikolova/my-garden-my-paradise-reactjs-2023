b59d9afca294db9512472bd4a4f4a22aef0196a3
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-04-08 18:50:59 +0100

    Update about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,22 +4,23 @@ export const About = () => {
             <div className="about-page">
                 <h1>About Us</h1>
                 <div className="content">
-                    
-                         <p className="paragraph">
-                            This blog is actually a collective site from more than one writer. 
-                         All posts are published by unprofessinal gardeners who suggested their ways of
-                         gardening. By sharing thier expirience, you can benefit from their posts and apply their advices in your work.
-                          If you are passionate about growing your own garden, or you have some ideas, you are more than welcome to join us and become a part of this community. </p>
-                    
-               
-                </div>
-                <div className="image">
-                <img src="/images/apple-blossom.jpg" alt="apple blossom" />
-              
+                    <h2>What we do is what we love</h2>
+                    <p className="paragraph">
+                        This blog is actually a collective site from more than one writer.
+                        All posts are published by unprofessinal gardeners who suggested their ways of
+                        gardening. By sharing thier expirience, you can benefit from their posts and apply their advices in your work.
+                        If you are passionate about growing your own garden, or you have some ideas, you are more than welcome to join us and become a part of this community. </p>
+                    <p className="paragraph">
+                        Here you can find inspirations, ideas, advice or relative resorces of professional gardening websites or simply a place to share your love to gardening.
+                    </p>
 
                 </div>
+                <div className="image">
+                    <img src="/images/apple-blossom.jpg" alt="apple blossom" />
+                </div>
                 <div className="content">
-                <p className="paragraph">The purpose of this blog is to gatter experienced and proven information of gardening  </p>
+                    <p className="paragraph">
+                        The purpose of this blog is to gatter experienced and proven information of gardening  </p>
                 </div>
             </div>
         </section>
